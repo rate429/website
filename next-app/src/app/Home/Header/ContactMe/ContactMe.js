d46ccbd6d5e8f7821d0caf26af1ccc88c30b1b14
Riley Rager <276484@gcloud.tisd.org>
2024-09-21 00:20:44 +0000

    codespace to local

--- next-app/src/app/Home/Header/ContactMe/ContactMe.js
+++ next-app/src/app/Home/Header/ContactMe/ContactMe.js
@@ -1,8 +1,9 @@
+import Image from 'next/image';
 import './css/ContactMe.css'
 
 
 export default function ContactMe() {
     return (<div className="ContactMe">
-        <button className="ContactMe ContactMeBtn">CONTACT ME</button>
+        <button className='ContactMe'><img src='/icons/github-mark.png'></img></button>
     </div>);
 }
